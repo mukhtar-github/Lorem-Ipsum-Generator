c75c9cdc7eac327d1f2be434d6cb83bfa781a616
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-22 11:56:54 +0100

    Update file

--- loremipsum-app/src/App.js
+++ loremipsum-app/src/App.js
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 
 function App() {
-  const = [count, setCount] = useState(0);
-  const = [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
   
   
   return (
